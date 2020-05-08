ebf3460140307c1c2159a6d53b1418a95cbd2904
Eda Nur Serttas (inf1364) <eda_nur.serttas@smail.th-koeln.de>
2020-05-08 18:54:06 +0200

    eingabe nur in Discord

---
+++ discord-test.js
@@ -0,0 +1,123 @@
+const readline = require('readline');
+var request = require('request')
+var amqp = require('amqplib/callback_api');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NzA2MTc5MDMxNzkxNDM1ODQ4.XrQGDw.sKF2YRmtqmQtiDVYE-jl6iNTEO4';
+// token Ende jl6iNTEO4
+
+
+/*
+*
+*
+* DIES IST EINE TESTDATEI // IN DER DATEI KANN MAN DIE EINGABE ÜBER DISCORD MACHEN
+*
+*
+*/
+
+
+let str
+
+amqp.connect('amqp://urqhfjsh:De4vJ6bu15evWfugZUfdgi2nxrVvUSun@kangaroo.rmq.cloudamqp.com/urqhfjsh', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var exchange = 'city';
+
+        channel.assertExchange(exchange, 'fanout', {
+            durable: false
+        });
+
+/*        rl.question('Bitte geben Sie ein Startort ein: ', (ans2) => {
+          rl.question('Bitte geben Sie ein Zielort ein: ', (ans) => {
+         //     channel.publish(exchange, ans, Buffer.from(ans));
+
+          rl.question('Bitte geben Sie [weather] für Wetterdaten oder [traffic] für Verkehrsinfos ein: ', (type) => {
+            if(type === 'weather'){
+              channel.publish(exchange, ans, Buffer.from(ans));
+              getWeather(ans, channel)
+            }else if(type === 'traffic'){ 
+              rl.question('Geben Sie ein Verkehrsmittel ein: '  , (ans3) => {   
+              channel.publish(exchange, ans, Buffer.from(`${ans2},${ans},${ans3}`))              
+              getMap(ans, ans2, ans3, channel)
+              })
+            }else{
+              console.log('');
+              console.log('Ungültige Eingabe! Bitte versuchen Sie es erneut.');
+              console.log('');
+              process.exit();
+            }
+          }); 
+       });       
+    })*/
+
+    client.login(token)
+
+    client.on('ready', () => {
+        console.log('Bot ist online!')
+    });
+    
+   client.on("message", function(message) {
+ //      console.log(message)
+        var msgArray = message.toString().split(' ')
+        msgArray[0] = '/subscribe'
+ //    if(msgArray[0] === '/subscribe' || msgArray[0] === '/abonnieren' || msgArray[0] === '/channel'){
+       const daten = {
+         Startort: msgArray[1],
+         Zielort: msgArray[2],
+         Vmittel: msgArray[3]
+       };
+
+       channel.publish(exchange, daten.Startort, Buffer.from(daten.Startort));
+
+       var exchange1 = 'weather-monitor';
+
+       /*    console.log('');
+           console.log('Jetzt kommen die Wetterdaten');
+           console.log('Sie haben ausgewählt: ' + daten.Startort);*/
+          
+           channel.assertExchange(exchange1, 'topic', {
+             durable: false
+           });
+       
+           channel.assertQueue('', {
+             exclusive: true
+           }, function(error2, q) {
+             if (error2) {
+                 throw error2;
+             }
+             console.log(' [*] Waiting for logs. To exit press CTRL+C');
+       
+             channel.bindQueue(q.queue, exchange1, daten.Startort);
+             
+             channel.publish(exchange1, daten.Startort, Buffer.from(daten.Startort));
+       
+             channel.consume(q.queue, function(msg) {
+                 console.log(msg)
+                 console.log(" [x] current weather in %s: '%s°C'", msg.fields.routingKey, msg.content.toString());
+           //      console.log(msg.toString())
+           
+       if(msgArray[0] + daten){
+            message.channel.send('Es wurden folgende Daten abgefragt: ' + msg.fields.routingKey )
+            message.channel.send('Zeit: '+ msg.content.toString()+' h')
+             .catch(console.error);
+       }
+   });
+    
+      }, {
+          noAck: true
+      });
+    });
+    
+});
+});
